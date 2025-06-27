b1583ca0afa419c1d3404b2a1f600b1a61a61472
Sameea Sarwari <ssarwari@c3r8s4.42urduliz.com>
2025-06-27 17:27:00 +0200

    smi

--- libftProject/ft_putstr_fd.c
+++ libftProject/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ssarwari <ssarwari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:15:47 by ssarwari          #+#    #+#             */
-/*   Updated: 2025/04/27 18:08:22 by ssarwari         ###   ########.fr       */
+/*   Updated: 2025/05/26 19:12:53 by ssarwari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-/* 
+
 int main(void)
 {
-    ft_putstr_fd("Hello, world!\n", 1);
+    ft_putstr_fd("Hello, world!\0 lala", 1);
     return (0);
-} */
+}
 /* cc -Wall -Wextra -Werror ft_putstr_fd.c && ./a.out */
